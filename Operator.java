98cff5b136f6fb9057cc6f4493ccaceeb423efb4
yigou <2080087797@qq.com>
2017-06-26 16:16:04 +0800

    3.Operator.java 2017/6/26

--- Operator.java
+++ Operator.java
@@ -1,43 +1,22 @@
+/**
+*  运算符
+*/
 public class Operator{
 	
 	public static void main(String[] args){
-		// char x='a';
-		// System.out.println(x);//a
-		// System.out.println(x+1);//98**************************
-		// System.out.println(x-32);//65
-		// System.out.println((char)(x-32));//A************************
+		char x='a';
+		System.out.println(x);//输出结果a
+		System.out.println(x+1);//输出结果98
+		System.out.println((char)(x-32));//输出结果A
 		
-		// int x1=10,x2=4;
-		// System.out.println(x1++*--x2+x2--);
-		// // 10*3+3=33
-		// System.out.println(x1--*++x2);
-		// // 10*5=50
-		// System.out.println(--x1/++x2*x2--);
-		// // 9/5*5=5
-		// System.out.println(++x2+x1--*++x1);//*******************************
-		// //5+10*10=105,5+10*11=115  
+		int x1=10,x2=4;
+		System.out.println(++x2+x1--*++x1);//输出结果5+10*10=105,5+10*11=115
 		
-		// int num1=8,num2=2;
-		// System.out.println(num1++>num2?num1:num2);
-		// //9
-		// System.out.println(num1--+num2);
-		// //10
-		// System.out.println(num1++ <<2);
-		// //32
-		// System.out.println(num1-->>3);
-		// //1
-		// System.out.println(num1--^num2);
-		// //10
-		// System.out.println(num1--^++num2);
-		// //11
-		
-		// int num3=101,num4=6;
-		// System.out.println(num3|num4);//***********************************
-		// //103
-		// System.out.println(num3&num4);//**************************************
-		// //4
-		// System.out.println(~num3);//**************************************
-		// //-102
+		int num3=101,num4=6;
+		System.out.println(num3|num4);//输出结果103
+		System.out.println(num3&num4);//输出结果4
+		System.out.println(~num3);//输出结果-102
+		System.out.println(num4^num3);//输出结果99，一样为1
 		
 	}
 }   
