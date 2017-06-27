0cc8ee2c91299c094c7fe661ba1cd76a01101ad1
yigou <2080087797@qq.com>
2017-06-27 22:11:39 +0800

    7.Password.java 2017/6/27

--- Password.java
+++ Password.java
@@ -1,5 +1,11 @@
+package newDay;
 import java.util.*;
+/**题目
+ * @author Cooper
+ * 密码强度
+ */
 public class Password{
+	
 	public static void main(String[] args){
 		Scanner input=new Scanner(System.in);
 		System.out.print("请输入你的密码：");
@@ -13,10 +19,7 @@ public class Password{
 			score+=25;
 		}
 		
-		
-		/**
-		*运用ASCII码表，判断String变量中有没有符号，小写字母，大写字母，数字
-		*/ 
+		/*运用ASCII码表，判断String变量中有没有符号，小写字母，大写字母，数字*/		
 		int sumXiao=0,sumDa=0,sumShu=0,sumFu=0;
 		for(int i=0;i<str.length();i++){
 			char c=str.charAt(i);
@@ -36,8 +39,8 @@ public class Password{
 				sumFu++;
 			}
 		}	
-			
 		
+		/*加分机制*/
 		if(sumDa>0 && sumXiao>0){
 			score+=20;
 		}
@@ -69,6 +72,7 @@ public class Password{
 			score+=5;
 		}
 		
+		/*输出*/
 		System.out.println(score);
 		System.out.println("你输入了"+sumDa+"个大写字母");
 		System.out.println("你输入了"+sumXiao+"个小写字母");
