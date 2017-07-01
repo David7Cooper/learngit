6cff92e0bbd0486ca5b0800efb4f9191ff9c0f93
yigou <2080087797@qq.com>
2017-07-01 10:38:10 +0800

    My_String.java 2017/7/1

--- My_String.java
+++ My_String.java
@@ -7,6 +7,9 @@ public class My_String {
 		my_contains();
 		my_charAt();
 		my2_charAt();
+		my3_charAt();
+		my_substring();
+		my_indexOf();
 	}
 	
 	/*从键盘输入一串字符串，如果包含1那就将这串字符串的字母全部转化为小写*/
@@ -49,6 +52,82 @@ public class My_String {
 		str = str.replace("a", "*");
 		System.out.println("该字符中字符为a的下标为"+s);
 		System.out.println(str);
+	}
+	
+	/*输入字符串，倒叙输出*/
+	public static void my3_charAt(){
+		Scanner input=new Scanner(System.in);
+		String str=input.next();
+		/*for(int i=1;i<str.length()-1;i++){
+			char ch=str.charAt(i);
+			str.charAt(i)=str.charAt(str.length()-i);
+			str.charAt(str.length()-i)=ch;//str.charAt(str.length()-i)不是一个变量，不可赋值
+		}*/
+		char[] ch=new char[str.length()];//动态初始化数组规定数组的长度为字符串的长度
+		for(int i=0;i<ch.length;i++){
+			ch[i]=str.charAt(i);//把String分割成字符放入字符数组ch中
+		}
+		for(int i=1;i<=(ch.length-1)/2;i++){//i<(ch.length-1)/2得出的范围对调不了偶数中间的的两个数
+			char temp=ch[i];
+			ch[i]=ch[ch.length-i-1];
+			ch[ch.length-i-1]=temp;
+		}
+		for(int i=0;i<ch.length;i++){
+			System.out.print(ch[i]);
+		}
+	}
+	
+	/*从键盘输入字符串，然后再输入你要查找的字符串，判断字符串的个数*/
+	public static void my_substring(){
+		Scanner input=new Scanner(System.in);
+		System.out.print("请输入字符串：");
+		String str=input.nextLine();
+		System.out.print("请输入你要查询的字符串：");
+		String str1=input.nextLine();
+		int count=0;
+		int i=0;
+		while(i<str.length()){
+			if(i>=str.length()-(str1.length()-1)){//i>=str.length()-(str1.length()-1)
+				break;	
+			}
+			if(add(str,str1,i)){
+				count++;
+				str=str.substring(i+str1.length());
+				i=0;
+				continue;//break直接跳出循环
+			}	
+			i++;
+		}
+		System.out.println("字符串"+"\t"+str1+"\t"+"的个数为"+count);	
+	}
+	/*my_substring()的内调方法*/
+	public static boolean add(String str,String str1,int i){
+		int count=0;
+		for(int j=0;j<str1.length();j++){
+			if(str.charAt(i+j)==str1.charAt(j)){
+				count++;
+			}
+		}
+		return count==str1.length();
+	}
+	
+	/*查找指定的任意字符串的个数，用indexOf()方法实现*/
+	public static void my_indexOf(){
+		Scanner input=new Scanner(System.in);
+		System.out.print("请输入你的字符串");
+		String str=input.nextLine();
+		System.out.print("请输入你要查询的字符串");
+		String str1=input.nextLine();
+		int num=0;
+		for(int i=0;i<str.length();){
+			if(str.indexOf(str1,i)!=-1){
+				i=str.indexOf(str1,i)+str1.length();
+				num++;
+			}else{
+				break;
+			}
+		}
+		System.out.println(num);
 	}
 	
 	
